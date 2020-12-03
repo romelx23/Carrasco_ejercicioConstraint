2d6a6b314d2a10a1dcbf7e3e44522013d316115f
Carrasco Romel <romelx23@gmail.com>
2020-12-03 16:54:10 -0500

    Ejercicio ConstraintLayout

---
+++ app/src/main/java/com/example/dam_semana_constrainlayout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.dam_semana_constrainlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
